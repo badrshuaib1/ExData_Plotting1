b5ec09f0e7220bd000a8be051b8d39d1dc7e1673
badrshuaib1 <34256292+badrshuaib1@users.noreply.github.com>
2018-04-15 22:03:43 -0700

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -19,6 +19,6 @@ with(dftemp,{
         plot(DateTime, Sub_metering_1,  type = "l",lty= 1) 
         lines(DateTime, Sub_metering_2,lty= 1, col= "red") 
         lines(DateTime, Sub_metering_3,lty= 1, col= "blue") 
-        legend("topright", pch = "_", col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+        legend("topright",  lty=1, lwd=2.5, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 })
 dev.off()
